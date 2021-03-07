ff00f45b6171500aeb05682f2b482b369dd45c6a
Anh-Thu Phan <aphan@ufl.edu>
2021-03-07 16:08:20 -0700

    Reveal sub menus on hover

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 // It is always helpful to use comments in your code!
+var navLabels = document.getElementsByClassName("wrapper");
+var dropdowns = document.getElementsByClassName("sub-menu");
+
+function toggleDropdown() {
+    this.children[1].classList.toggle("show");
+}
+
+for(i=0; i < navLabels.length; i++) {
+    navLabels[i].addEventListener("mouseenter", toggleDropdown);
+    navLabels[i].addEventListener("mouseleave", toggleDropdown);
+}
